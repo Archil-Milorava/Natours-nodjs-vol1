6eedbb7188234c6d9c2065e34a0f6bf70bf83df0
Archil-Milorava <achimilorava16@gmail.com>
2024-09-07 17:08:06 +0400

    middlewares explained

---
+++ routes/toursRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const toursController = require('./../controllers/toursControllers')
+
+const router = express.Router();
+
+router.param('id', toursController.checkId)
+
+router.route('/').get(toursController.getAllTours).post(toursController.checkBody, toursController.createTour);
+
+router
+  .route('/:id')
+  .get(toursController.getTour)
+  .patch(toursController.updateTour)
+  .delete(toursController.deleteTour);
+
+  module.exports = router;
---
+++ server.js
@@ -0,0 +1,9 @@
+
+const app = require('./app')
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`the server is running on port ${PORT}`);
+  });
+  
